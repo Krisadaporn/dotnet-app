01c2ab4c741238b0c992e885f8e73b072ef0b547
Krisadaporn Petchdee <krisadaporn.p@gmail.com>
2025-02-17 13:46:48 +0700

    update code

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // Keep the console window open until a key is pressed
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
