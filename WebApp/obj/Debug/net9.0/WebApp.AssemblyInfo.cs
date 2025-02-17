ddf673dcebac8b53fb05b956a929e4ecb02ae5c0
Krisadaporn Petchdee <krisadaporn.p@gmail.com>
2025-02-17 14:51:53 +0700

    remove app

--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac42a43b0523e4edf583ffc55e17f93d30c57298")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+216ac7a9db180e5323802f28bf63c440c725cc7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
